1d15fc70dc180e2ddfc9ee911780a92b52b8f729
Nathan Zeng <nathan.j.zeng@gmail.com>
2025-12-15 18:23:53 -0800

    update todo

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,12 +8,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- TODO: this isn't working in iTerm, need to use <C-\><C-n>
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Left and right arrow in normal mode scroll the page
