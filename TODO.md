b62843a69ea7e0c69402dae6051b51c940ba45f5
Nathan Zeng <nathan.j.zeng@gmail.com>
2025-12-16 17:56:05 -0800

    mini terminal help and tree api keybinds

--- TODO.md
+++ TODO.md
@@ -1,4 +1,8 @@
 modularize lazy-plugins
+ 
+We need a keybind for :NvimTreeFindFile it's like the little compass thing in php storm
+
+Indent needs to be less
 
 I remember seeing a quote/paren/bracket autocomplete that I wanted.
 It's called mini.pairs
--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -6,8 +6,20 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('nvim-tree').setup {
-      vim.keymap.set('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>', { desc = '[T]ree' }),
-    }
+    require('nvim-tree').setup {}
+    -- Don't do random things in the setup table, do it in the config function instead
+    local api = require 'nvim-tree.api'
+
+    -- Toggle the tree
+    vim.keymap.set('n', '<leader>tt', api.tree.toggle, { desc = 'Toggle [T]ree' })
+
+    -- Increase and decrease tree width
+    vim.keymap.set('n', '<leader>t<right>', function()
+      api.tree.resize { relative = 25 }
+    end, { desc = 'Increase tree width' })
+
+    vim.keymap.set('n', '<leader>t<left>', function()
+      api.tree.resize { relative = -25 }
+    end, { desc = 'Decrease tree width' })
   end,
 }
