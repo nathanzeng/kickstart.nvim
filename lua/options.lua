a0764c405e518c837ab2ac44cc1038ef68b72952
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-01-11 15:48:35 -0800

    centralize gitsigns code

--- lua/options.lua
+++ lua/options.lua
@@ -63,6 +63,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.tabstop = 4
 
 -- Don't have `o` add a comment
+-- TODO: this isnt actually working, have to source the file to make it work
 vim.opt.formatoptions:remove 'o'
 
 -- Preview substitutions live, as you type!
@@ -75,8 +76,6 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
-
--- TODO: I have no idea what modeline does
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
