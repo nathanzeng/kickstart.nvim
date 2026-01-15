1a6d8f74f4c7949440285ed2550a6fba39b4a1ee
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-01-15 14:15:38 -0800

    toggle the diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -2,6 +2,17 @@ return {
   'sindrets/diffview.nvim',
   config = function()
     require('diffview').setup {}
-    vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<CR>', { desc = '[d]iff' })
+    local diffViewIsOpen = false
+
+    local function toggle_diffview()
+      if diffViewIsOpen then
+        vim.cmd 'DiffviewClose'
+        diffViewIsOpen = false
+      else
+        vim.cmd 'DiffviewOpen'
+        diffViewIsOpen = true
+      end
+    end
+    vim.keymap.set('n', '<leader>gd', toggle_diffview, { desc = '[d]iff' })
   end,
 }
