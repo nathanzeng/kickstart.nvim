c5cd0028dbc306015ffbb6552fbea7569f3681fc
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-01-16 09:26:06 -0800

    s for style format conform

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -5,12 +5,12 @@ return {
   cmd = { 'ConformInfo' },
   keys = {
     {
-      '<leader>f',
+      '<leader>s',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
       mode = '',
-      desc = '[F]ormat buffer',
+      desc = '[s]tyle format buffer',
     },
   },
   opts = {
