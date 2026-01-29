4510a02dcd9171e1875fc79ca9543bcdb8f9c468
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-01-28 20:26:28 -0800

    (guess-indent): delete

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -10,19 +10,14 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
   --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.which-key',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns',
   -- require 'kickstart.plugins.lint',
 
   { import = 'custom.plugins' },
